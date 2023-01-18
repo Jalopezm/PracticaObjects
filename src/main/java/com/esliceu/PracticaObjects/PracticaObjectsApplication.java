def5be55620bfc684c6b7095aea4625f489c9d9b
Jalopezm <jalopezm@esliceu.net>
2023-01-18 18:37:59 +0100

    First Commit

---
+++ src/main/java/com/esliceu/PracticaObjects/PracticaObjectsApplication.java
@@ -0,0 +1,13 @@
+package com.esliceu.PracticaObjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaObjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaObjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esliceu/PracticaObjects/PracticaObjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esliceu.PracticaObjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaObjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
